ab96c3c7a10b4200ff200146cff3c5e5cd80e5f6
Mauro-Rizzi <mrizzi@fi.uba.ar>
2021-02-21 20:51:14 -0300

    Refactors CreditCard Cambios Minimos en formato de archivo

--- 09-TusLibros/CreditCard.py
+++ 09-TusLibros/CreditCard.py
@@ -1,7 +1,6 @@
 class CreditCard:
     
     def __init__(self, owners_name, credit_card_number, expiration_month_of_year):
-        
         self._check_owners_name_is_not_blank(owners_name)
         self._check_valid_credit_card_number(credit_card_number)
 
@@ -11,15 +10,14 @@ class CreditCard:
 
     def _check_owners_name_is_not_blank(self, owners_name):
         if owners_name == '':
-            raise Exception(cls.name_cannot_be_blank_error_message())
+            raise Exception(self.__class__.name_cannot_be_blank_error_message())
     
     def _check_valid_credit_card_number(self, credit_card_number):
         if len(credit_card_number) != 16 or not credit_card_number.isnumeric():
-            raise Exception(cls.number_must_be_sixteen_digits_error_message())
+            raise Exception(self.__class__.number_must_be_sixteen_digits_error_message())
 
     def is_expired_on(self, a_datetime):
         return a_datetime.date() > self._expiration_month_of_year.last_date()
-
 
     @classmethod
     def name_cannot_be_blank_error_message(cls):
@@ -28,4 +26,3 @@ class CreditCard:
     @classmethod
     def number_must_be_sixteen_digits_error_message(cls):
         return "El numero de la tarjeta de credito es invalido"
-    
