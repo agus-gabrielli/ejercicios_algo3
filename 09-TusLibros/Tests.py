88c7ee2c5c3e4cc3e99f5dec75f6a6c7110e1598
Mauro-Rizzi <mrizzi@fi.uba.ar>
2021-02-21 21:09:25 -0300

    Se genera archivo MockMerchantProcessor Se mueve MockMerchantProcessor a nuevo archivo para seguir la recomendacion de la catedra sobre las clases

--- 09-TusLibros/Tests.py
+++ 09-TusLibros/Tests.py
@@ -8,15 +8,6 @@ from PublisherTestObjectsFactory import *
 import unittest
 
 
-class MockMerchantProcessor:
-    def process_payment(self, total, credit_card):
-        if credit_card._credit_card_number == "4380500008685118":
-            raise Exception(self.__class__.generic_rejected_payment_error_message())
-
-    @classmethod
-    def generic_rejected_payment_error_message(cls):
-        return "The payment was rejected"
-
 #####################################################################
 #                                                                   #
 #                       CLASE TESTCASE                              #
